0e592abcb97ae310875572fe15add9a52d19caf5
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-04-16 19:43:38 +0300

    Add simple Maven project

---
+++ configuration/src/test/java/net/petrikainulainen/wiremock/configuration/ExampleTest.java
@@ -0,0 +1,15 @@
+package net.petrikainulainen.wiremock.configuration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExampleTest {
+
+    @Test
+    @DisplayName("a placeholder test")
+    void placeholderTest() {
+        assertTrue(true);
+    }
+}
